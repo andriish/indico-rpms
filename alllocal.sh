1a7c937f9aaab352e141a0500d0cbe1b7430b69c
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2022-04-21 15:06:55 +0200

    OK

--- alllocal.sh
+++ alllocal.sh
@@ -8,7 +8,9 @@ declare -a BUILDLIST=(
 #pyparsing:3.0.8
 #Rivet:3.1.4
 #python-webargs:8.1.0
-python-hiredis:2.0.0
+#python-hiredis:2.0.0
+### python-pywebpack:1.2.0
+### python-pynpm:0.1.2
 #python-indico-fonts:1.1
 )
 
