fffc3c762202f519c12a0f63dfa2ea079e680c09
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2022-09-28 14:41:06 +0200

    OK

--- .github/workflows/fedora36/entrypoint.sh
+++ .github/workflows/fedora36/entrypoint.sh
@@ -2,7 +2,7 @@
 set -x
 uname -a 
 cat /etc/issue
-dnf -y install rpm-build git wget sed
+dnf -y install rpm-build git wget sed createrepo
 
 export PATH=$PATH:$(pwd)
 #dnf list installed | grep @cop | tr -s ' '| cut -f 1,2 -d' ' | sed 's/\.noarch//g' | sed 's/\.x86_64//g' | sed 's/\-[123456789]*\.fc36//g'  | sed 's/\ /:/g' |  sed 's/python3/python/g' | grep -v plugin | sed 's/marshmallow-o/marshmallow_o/g' | sed 's/marshmallow-s/marshmallow_s/g' | sed 's/iso-/iso_/g'     | sed 's/-validator/_validator/g' 
@@ -22,7 +22,6 @@ python-flask-marshmallow:0.14.0
 python-flask-url-map-serializer:0.0.1
 python-flask-webpackext:1.0.2
 python-hiredis:2.0.0
-#python-indico:3.2
 python-indico-fonts:1.1
 python-iso_4217:0.4.220401
 python-limits:2.5.2
@@ -69,13 +68,23 @@ fi
 done
 
 yum -y install wget $(rpmspec -P python-indico/3.2/*.spec | grep BuildRequires | tr -s ' ' |cut -d: -f2 | xargs) --skip-broken
-sh srpmsbuild.sh  python-indico 3.2
+sh srpmsbuild.sh  python-indico 3.2 --build 
 yum -y install wget  python-indico/3.2/rpmbuild/RPMS/*/*.rpm --skip-broken
 
 
 yum -y install wget $(rpmspec -P indico-mpp/3.2/*.spec | grep BuildRequires | tr -s ' ' |cut -d: -f2 | xargs) --skip-broken
-sh srpmsbuild.sh  indico-mpp 3.2
+sh srpmsbuild.sh  indico-mpp 3.2 --build 
 yum -y install wget  indico-mpp/3.2/rpmbuild/RPMS/*/*.rpm --skip-broken
+
+
+mkdir REPO -p
+cp ./*/*/rpmbuild/RPMS/*/*.rpm  REPO
+cd REPO
+ls -lah 
+createrepo  . 
+ls -lah 
+cd  ..
+
 
 out=$?
 echo ::set-output name=out::$out
