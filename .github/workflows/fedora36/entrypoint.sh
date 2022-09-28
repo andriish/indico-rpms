ddc45d07f7b78511fdbca811b8299903309d5141
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2022-09-28 13:08:57 +0200

    OK

--- .github/workflows/fedora36/entrypoint.sh
+++ .github/workflows/fedora36/entrypoint.sh
@@ -5,7 +5,7 @@ cat /etc/issue
 dnf -y install rpm-build git wget sed
 
 export PATH=$PATH:$(pwd)
-#dnf list installed | grep @cop | tr -s ' '| cut -f 1,2 -d' ' | sed 's/\.noarch//g' | sed 's/\.x86_64//g' | sed 's/\-[123456789]*\.fc36//g'  | sed 's/\ /:/g' | sed 's/_/-/g'| sed 's/python3/python/g'
+#dnf list installed | grep @cop | tr -s ' '| cut -f 1,2 -d' ' | sed 's/\.noarch//g' | sed 's/\.x86_64//g' | sed 's/\-[123456789]*\.fc36//g'  | sed 's/\ /:/g' |  sed 's/python3/python/g' | grep -v plugin | sed 's/marshmallow-/marshmallow_/g' | sed 's/iso-/iso_/g'   | sed 's/-SQLAlchemy/_SQLAlchemy/g'  | sed 's/-validator/_validator/g' 
 
 set -x 
 declare -a BUILDLIST=(
@@ -15,7 +15,7 @@ python-Flask-Limiter:2.4.0
 python-Flask-Multipass:0.4.6
 python-Flask-PluginEngine:0.4
 python-PyPDF2:2.11.0
-python-WTForms-SQLAlchemy:0.3
+python-WTForms_SQLAlchemy:0.3
 python-WTForms-dateutil:0.1
 python-bleach:5.0.1
 python-captcha:0.4
@@ -25,11 +25,11 @@ python-flask-webpackext:1.0.2
 python-hiredis:2.0.0
 python-indico:3.2
 python-indico-fonts:1.1
-python-iso-4217:0.4.220401
+python-iso_4217:0.4.220401
 python-limits:2.5.2
-python-marshmallow-oneofschema:3.0.1
-python-marshmallow-sqlalchemy:0.28.0
-python-marshmallow-dataclass:8.5.3
+python-marshmallow_oneofschema:3.0.1
+python-marshmallow_sqlalchemy:0.28.0
+python-marshmallow_dataclass:8.5.3
 python-pynpm:0.1.2
 python-pywebpack:1.2.0
 python-stack-data:0.5.0
