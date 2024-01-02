13930504c5407f6f9674bfaec7ecf6e9a0d0c308
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2024-01-02 03:27:10 +0100

    OK

--- .github/workflows/fedora39/entrypoint.sh
+++ .github/workflows/fedora39/entrypoint.sh
@@ -4,14 +4,14 @@ uname -a
 cat /etc/issue
 
 dnf -y install dnf* rpm-build git wget sed createrepo
-dnf -y copr enable averbyts/IR 
+dnf -y copr enable averbyts/I328
 dnf -y install indico-devel
 export PATH=$PATH:$(pwd)
 set -x 
 mkdir -p /github/home/.npm
 chown -R 1001:121 "/github/home/.npm"
-sh srpmsbuild.sh  python-indico 3.2 --build 
-yum -y install wget  python-indico/3.2/rpmbuild/RPMS/*/*.rpm --skip-broken
+sh srpmsbuild.sh  python-indico 3.2.8 --build 
+yum -y install wget  python-indico/3.2.8/rpmbuild/RPMS/*/*.rpm --skip-broken
 
 out=$?
 echo ::set-output name=out::$out
