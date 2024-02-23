6d1b205627a91e4476690d597b1249dc66e4de7d
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2024-02-23 08:21:00 +0100

    OK

--- debianalllocal.sh
+++ debianalllocal.sh
@@ -16,7 +16,7 @@ pywebpack:1.2.0:pywebpack
 indico-fonts:1.2:indico-fonts
 semver:3.0.2:semver
 flask-multipass:0.5.2:Flask-Multipass
-#flask-url-map-serializer:0.0.1:flask-url-map-serializer
+flask-url-map-serializer:0.1.0:flask-url-map-serializer
 flask-webpackext:1.0.2:flask-webpackext
 wtforms-dateutil:0.1:WTForms-dateutil
 wtforms-sqlalchemy:0.4.1:WTForms-SQLAlchemy
@@ -32,6 +32,9 @@ v=$(echo $a | cut -f2 -d: )
 
 (
 u=https://pypi.io/packages/source/$(echo $p | head -c 1)/$p/$P-$v.tar.gz
+if [ "$p" = "flask-url-map-serializer" ]; then
+u=https://github.com/indico/js-flask-urls/archive/refs/tags/babel-plugin-flask-urls@$v.tar.gz
+fi
 cd $p/$v
 TOP=$(pwd)
 rm -rf mydbtop
@@ -40,7 +43,11 @@ cd mydbtop/BUILD
 cp -r $TOP/debian ./
 wget $u -O ../$p"_"$v.orig.tar.gz
 tar zxfv ../$p"_"$v.orig.tar.gz 
+if [ "$p" = "flask-url-map-serializer" ]; then
+mv js-flask-urls-babel-plugin-flask-urls-$v/* ./
+else
 mv $P-$v/* ./
+fi
 dpkg-buildpackage -us -uc
 cd $TTOP
 apt-get -y install ./$p/$v/mydbtop/*deb
