50e9ea630dc9c7c5f3025a94997e38a514720619
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2024-02-29 21:46:08 +0100

    Try to adopt Debian testing

--- RPM/rpm_alllocal.sh
+++ RPM/rpm_alllocal.sh
@@ -24,12 +24,13 @@ python-webargs:8.3.0
 python-indico:3.3.0
 python-indico-mpp-configuration:3.3
 )
-declare -a BUILDLIST=(
+exit 0
+#declare -a BUILDLIST=(
 #python-pypdf:4.0.1
 #python-indico-mpp-configuration:3.3
 #python-iso4217:1.11.20220401
-python-indico:3.3.0
-)
+#python-indico:3.3.0
+#)
 for a in "${BUILDLIST[@]}" 
 do
 p=$(echo $a | cut -f1 -d: )
