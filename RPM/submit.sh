2f1f950c81464c2591feb69f70ee1492faf0f4ad
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2024-04-15 19:28:05 +0200

    OK

--- RPM/submit.sh
+++ RPM/submit.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
 export PATH=$PATH:$(pwd)
-copr-cli create --enable-net=on --chroot fedora-39-x86_64 --chroot fedora-rawhide-x86_64 I330
+copr-cli create --enable-net=on --chroot fedora-40-x86_64 --chroot fedora-rawhide-x86_64 I331
 declare -a BUILDLIST=( 
 python-exceptiongroup:1.2.0
 python-indico-mpp-configuration:3.3
-python-indico:3.3.0
+python-indico:3.3.1
 python-Flask-Limiter:3.5.0
 python-Flask-Multipass:0.5.3
 python-Flask-PluginEngine:0.5
@@ -32,15 +32,15 @@ export name=$(echo $a | cut -f1 -d: )
 export version=$(echo $a | cut -f2 -d: )
 envsubst <<EOF > temp.sh
 #!/bin/bash
-git clone --depth 3 https://github.com/andriish/indico-rpms.git -b indico330pre
+git clone --depth 3 https://github.com/andriish/indico-rpms.git -b indico331
 cd indico-rpms/RPM
 sh srpmsbuild.sh  $name $version
 EOF
-copr add-package-custom I330 \
+copr add-package-custom I331 \
         --name $name \
         --script temp.sh \
         --script-resultdir indico-rpms/RPM/$name/$version/rpmbuild/SOURCES/ \
         --script-builddeps 'git rpmdevtools wget' \
-        --script-chroot fedora-39-x86_64
+        --script-chroot fedora-40-x86_64
 mv temp.sh log/$name$version
 done
