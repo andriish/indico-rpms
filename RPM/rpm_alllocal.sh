eb8d15b55259962202676444d86455c28dac9525
Your Name <you@example.com>
2025-06-21 11:53:03 +0200

    Updates

--- RPM/rpm_alllocal.sh
+++ RPM/rpm_alllocal.sh
@@ -25,7 +25,7 @@ python-wtforms-sqlalchemy:0.3.0
 python-indico:3.3.6
 python-indico-mpp-configuration:3.3
 )
-declare -a BUILDLIST=(
+declare -a BUILDLISTX=(
 #python-limits:5.4.0  # etdc problems
 #python-Flask-Multipass:0.10  # OK
 #python-captcha:0.7.1  # OK
@@ -64,6 +64,32 @@ python-wtforms-sqlalchemy:0.4.2
 
 #python-indico:3.3.6
 #python-indico-mpp-configuration:3.3
+
+
+declare -a BUILDLIST=(
+python-wallet-py3k:0.0.4
+python-limits:3.9.0
+python-Flask-Limiter:3.12
+python-Flask-Multipass:0.10 
+python-Flask-PluginEngine:0.5
+python-WTForms-dateutil:0.1
+python-captcha:0.7.1
+python-flask-marshmallow:1.3.0
+python-flask-url-map-serializer:0.1.0
+python-indico-fonts:1.2
+python-iso4217:1.13.20250512
+python-marshmallow_oneofschema:3.2.0
+python-marshmallow_sqlalchemy:1.4.2
+python-marshmallow-dataclass:8.7.1
+python-pynpm:0.3.0
+python-pywebpack:2.1.0
+python-flask-webpackext:2.1.0
+python-webargs:8.7.0
+python-wtforms-sqlalchemy:0.4.2
+python-indico:3.3.6
+python-indico-mpp-configuration:3.3
+)
+
 
 
 #mkdir -p python-indico/3.3.0/rpmbuild/RPMS/noarch
