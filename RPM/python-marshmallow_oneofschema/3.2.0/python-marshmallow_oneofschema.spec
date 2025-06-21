b46f864cba4379a514a1585b964561807bb95e5c
Your Name <you@example.com>
2025-06-21 11:49:54 +0200

    Updates

---
+++ RPM/python-marshmallow_oneofschema/3.2.0/python-marshmallow_oneofschema.spec
@@ -0,0 +1,49 @@
+%global srcname marshmallow-oneofschema
+%global srcnamenu marshmallow_oneofschema
+
+Name:           python-%{srcname}
+Version:        3.2.0
+Release:        1%{?dist}
+Summary:        An extension to marshmallow to support schema (de)multiplexing.
+
+License:        MIT
+URL:            http://marshmallow.readthedocs.org/en/latest/
+Source:         https://github.com/marshmallow-code/marshmallow-oneofschema/archive/refs/tags/3.2.0.tar.gz
+BuildArch:      noarch
+BuildRequires:  python3-pip python3-wheel
+
+%global _description %{expand:
+This library adds a special kind of schema that actually multiplexes 
+other schemas based on object type. When serializing values, it uses 
+get_obj_type() method to get object type name. Then it uses type_schemas 
+name-to-Schema mapping to get schema for that particular object type, 
+serializes object using that schema and adds an extra field with name
+ of object type. Deserialization is reverse..}
+
+%description %_description
+
+%package -n python3-%{srcname}
+Summary:        %{summary}
+BuildRequires:  python3-devel
+BuildRequires:  python3-setuptools
+
+%description -n python3-%{srcname} %_description
+
+%prep
+%autosetup -n %{srcname}-%{version}
+
+%build
+%pyproject_wheel
+
+%install
+%pyproject_install
+
+# Note that there is no %%files section for the unversioned python module
+%files -n python3-%{srcname}
+
+%{python3_sitelib}/%{srcnamenu}-*.dist-info/
+%{python3_sitelib}/%{srcnamenu}/
+
+%changelog
+* Thu Sep 29 2022 Andrii Verbytskyi andrii.verbytskyi@mpp.mpg.de>
+- Cleanup 
--- RPM/rpm_alllocal.sh
+++ RPM/rpm_alllocal.sh
@@ -2,7 +2,7 @@
 
 export PATH=$PATH:$(pwd)
 set -x 
-declare -a BUILDLIST=(
+declare -a BUILDLISTO=(
 python-wallet-py3k:0.0.4
 python-limits:3.9.0
 python-Flask-Limiter:3.5.0
@@ -25,6 +25,47 @@ python-wtforms-sqlalchemy:0.3.0
 python-indico:3.3.6
 python-indico-mpp-configuration:3.3
 )
+declare -a BUILDLIST=(
+#python-limits:5.4.0  # etdc problems
+#python-Flask-Multipass:0.10  # OK
+#python-captcha:0.7.1  # OK
+#python-Flask-Limiter:3.12 #OK
+#python-flask-marshmallow:1.3.0 #OK
+#python-iso4217:1.13.20250512 #OK
+#python-marshmallow_oneofschema:3.2.0 #OK
+#python-marshmallow_sqlalchemy:1.4.2 #OK
+#python-marshmallow-dataclass:8.7.1 #OK
+#python-pywebpack:2.1.0  #OK
+#python-pynpm:0.3.0  #OK
+#python-flask-webpackext:2.1.0 #OK
+#python-webargs:8.7.0 #OK
+python-wtforms-sqlalchemy:0.4.2
+)
+#python-wallet-py3k:0.0.4                # OK
+#python-limits:3.9.0                     # 5.4.0
+#python-Flask-Limiter:3.5.0                  # 3.12
+#python-Flask-Multipass:0.5.3                # 0.10 
+#python-Flask-PluginEngine:0.5               # OK
+#python-WTForms-dateutil:0.1                 #OK
+#python-captcha:0.5.0                        # 0.7.1
+#python-flask-marshmallow:0.15.0             # 1.3.0
+#python-flask-url-map-serializer:0.1.0       # OK, NOT ON PYPY
+#python-indico-fonts:1.2
+#python-iso4217:1.11.20220401                #1.13.20250512
+#python-marshmallow_oneofschema:3.0.1        # 3.2.0  NOT ON PYPY
+#python-marshmallow_sqlalchemy:0.29.0        # 1.4.2
+#python-marshmallow-dataclass:8.6.0          # 8.7.1
+#python-pynpm:0.2.0                          # 0.3.0
+#python-pywebpack:2.0.0                      # 2.1.0
+#python-flask-webpackext:1.0.2               # 2.1.0
+
+#python-webargs:8.3.0                        # 8.7.0
+#python-wtforms-sqlalchemy:0.3.0             # 0.4.2
+
+#python-indico:3.3.6
+#python-indico-mpp-configuration:3.3
+
+
 #mkdir -p python-indico/3.3.0/rpmbuild/RPMS/noarch
 #touch python-indico/3.3.0/rpmbuild/RPMS/noarch/1.rpm
 #exit 0
