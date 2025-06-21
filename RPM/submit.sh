41bad6456207ef7b032b339f5ab2c456ea554388
Your Name <you@example.com>
2025-06-21 12:07:51 +0200

    Updates

--- RPM/submit.sh
+++ RPM/submit.sh
@@ -3,27 +3,27 @@
 export PATH=$PATH:$(pwd)
 copr-cli create --enable-net=on --chroot fedora-42-x86_64 --chroot fedora-rawhide-x86_64 I336
 declare -a BUILDLIST=( 
-python-indico-mpp-configuration:3.3
-python-indico:3.3.6
-python-Flask-Limiter:3.5.0
-python-Flask-Multipass:0.5.3
+python-wallet-py3k:0.0.4
+python-limits:3.9.0
+python-Flask-Limiter:3.12
+python-Flask-Multipass:0.10 
 python-Flask-PluginEngine:0.5
 python-WTForms-dateutil:0.1
-python-captcha:0.5.0
-python-flask-marshmallow:0.15.0
+python-captcha:0.7.1
+python-flask-marshmallow:1.3.0
 python-flask-url-map-serializer:0.1.0
-python-flask-webpackext:1.0.2
 python-indico-fonts:1.2
-python-iso4217:1.11.20220401
-python-marshmallow_oneofschema:3.0.1
-python-marshmallow_sqlalchemy:0.29.0
-python-marshmallow-dataclass:8.6.0
-python-pynpm:0.2.0
-python-pywebpack:2.0.0
-python-webargs:8.3.0
-python-wtforms-sqlalchemy:0.3.0
-python-limits:3.9.0
-python-wallet-py3k:0.0.4
+python-iso4217:1.13.20250512
+python-marshmallow_oneofschema:3.2.0
+python-marshmallow_sqlalchemy:1.4.2
+python-marshmallow-dataclass:8.7.1
+python-pynpm:0.3.0
+python-pywebpack:2.1.0
+python-flask-webpackext:2.1.0
+python-webargs:8.7.0
+python-wtforms-sqlalchemy:0.4.2
+python-indico:3.3.6
+python-indico-mpp-configuration:3.3
 )
 
 mkdir -p log
