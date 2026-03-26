98010e2ecdd66e64dfafd519e2b131e67837b9c3
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2026-03-26 17:41:25 +0100

    OK

--- RPM/rpm_alllocal.sh
+++ RPM/rpm_alllocal.sh
@@ -101,7 +101,8 @@ declare -a BUILDLIST=(
 #python-marshmallow-enum:1.5.1
 #python-pyrsistent:0.20.0
 #python-Flask-Multipass:0.11.2
-python-pywebpack:2.2.1
+#python-pywebpack:2.2.1
+python-iso4217:1.16
 )
 #python-pypdf:4.0.1
 #python-indico-mpp-configuration:3.3
