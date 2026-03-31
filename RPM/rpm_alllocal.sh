905bc15f356b59622c375c143e61fadc61990380
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2026-03-31 12:40:53 +0200

    Drop unused packages

--- RPM/rpm_alllocal.sh
+++ RPM/rpm_alllocal.sh
@@ -2,7 +2,7 @@
 
 export PATH=$PATH:$(pwd)
 set -x 
-declare -a BUILDLISTO=(
+declare -a BUILDLIST=(
 python-wallet-py3k:0.0.4
 python-limits:3.9.0
 python-Flask-Limiter:3.5.0
@@ -77,18 +77,19 @@ python-captcha:0.7.1
 python-flask-marshmallow:1.4.0
 python-flask-url-map-serializer:0.1.0
 python-indico-fonts:1.2
-python-iso4217:1.14
+python-iso4217:1.16
 python-marshmallow_oneofschema:3.2.0
 python-marshmallow_sqlalchemy:1.4.2
 python-marshmallow-dataclass:8.7.1
 python-pynpm:0.3.0
-python-pywebpack:2.1.0
+python-pywebpack:2.2.1
 python-flask-webpackext:2.1.0
-python-webargs:8.7.0
+python-webargs:8.7.1
 python-wtforms-sqlalchemy:0.3.0
-python-indico:3.3.6
+python-indico:3.3.12
 python-indico-mpp-configuration:3.3
 python-pytest-runner:4.0
+python-marshmallow-enum:1.5.1
 )
 
 
@@ -103,7 +104,7 @@ declare -a BUILDLIST=(
 #python-Flask-Multipass:0.11.2
 #python-pywebpack:2.2.1
 #python-webargs:8.7.1
-python-pyrsistent:0.21.0
+#python-pyrsistent:0.21.0
 )
 #python-pypdf:4.0.1
 #python-indico-mpp-configuration:3.3
